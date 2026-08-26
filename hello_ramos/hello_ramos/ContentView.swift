7c1f14c6a11f24a209a235ff92ecf99df37154d4
TruNorth Engineering <trunortheng@gmail.com>
2026-08-25 22:42:45 -0700

    hello_ramos upload

---
+++ hello_ramos/hello_ramos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello_ramos
+//
+//  Created by cisstudent on 8/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello_ramos/hello_ramos/hello_ramosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_ramosApp.swift
+//  hello_ramos
+//
+//  Created by cisstudent on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct hello_ramosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
