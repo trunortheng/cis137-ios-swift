b9017f62124832f352fdde6a4131ceff4fe202ea
TruNorth Engineering <trunortheng@gmail.com>
2026-09-21 23:30:28 -0700

    Uploading assignment 5, tested with Xcode

---
+++ assignment5/assignment5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  assignment5
+//
+//  Created by cisstudent on 9/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ assignment5/assignment5/assignment5App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment5App.swift
+//  assignment5
+//
+//  Created by cisstudent on 9/21/26.
+//
+
+import SwiftUI
+
+@main
+struct assignment5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
