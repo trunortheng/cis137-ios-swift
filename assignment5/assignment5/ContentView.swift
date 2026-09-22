929581ec16940457e81437c48b2f0bb50ad1e7ae
TruNorth Engineering <trunortheng@gmail.com>
2026-09-21 23:32:28 -0700

    Uploading app changes

--- assignment5/assignment5/ContentView.swift
+++ assignment5/assignment5/ContentView.swift
@@ -1,8 +1,8 @@
 //
 //  ContentView.swift
-//  assignment5
-//
-//  Created by cisstudent on 9/21/26.
+//  Assignment 5: First iOS App
+//  Created by Erick Ramos
+//  Date: 09/20/2026
 //
 
 import SwiftUI
@@ -10,10 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            Text("Oh hey there! I'm Erick!")
+                .font(.title)
+//                .foregroundColor(.black)
+                .foregroundStyle(.blue.gradient)
+                .frame(width: 300)
+            
+            Image("profile")
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay {
+                    Circle()
+                        .stroke(.white, lineWidth: 5)
+                }
+                .shadow(radius: 7)
         }
         .padding()
     }
